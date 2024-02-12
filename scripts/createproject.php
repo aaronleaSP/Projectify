5f1c1109691c7e7dd51b52cd8e7d20aedff13e6e
Aaron Lea <aaronlea.22@ichat.sp.edu.sg>
2024-02-12 18:23:01 +0800

    Implemented permissions, delete task and modify task status

--- scripts/createproject.php
+++ scripts/createproject.php
@@ -15,17 +15,27 @@ if (!$conn) {
 }
 
 if (isset($_POST["projectname"]) && isset($_POST["projectdescription"])) {
+    $redirect = "<meta http-equiv='refresh' content='3;URL=../dashboard.php'><p/>Redirecting you back to dashboard in 3 seconds...";
+
     $projectname = $_POST["projectname"];
     $projectdesc = $_POST["projectdescription"];
+
+    $username = $_POST["createProjectUser"];
+    $email = $_POST["createProjectEmail"];
 
     $sql = "INSERT INTO projects_table (project_name, project_desc) VALUES ('$projectname', '$projectdesc')";
     if (mysqli_query($conn, $sql)) {
         $projectid = mysqli_insert_id($conn);
 
-        header("Location: ../project.php?id=$projectid&name=$projectname");
-        exit();
+        $sql = "INSERT INTO permissions_table (project_id, user_email, permission_type) VALUES ('$projectid', '$email', 'Owner')";
+
+        if (mysqli_query($conn, $sql)) {
+            header("Location: ../project.php?id=$projectid&name=$projectname");
+            exit();
+        } else {
+            die("Create permission failed: " .  mysqli_error($conn) . $redirect);
+        }
     } else {
-        $redirect = "<meta http-equiv='refresh' content='3;URL=dashboard.php'><p/>Redirecting you back to dashboard in 3 seconds...";
         die("Create project failed: " . mysqli_error($conn) . $redirect);
     }
 }
